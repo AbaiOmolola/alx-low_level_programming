68bccab8eb15d871cc3dab3cf1257439f570201e
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-02 09:28:13 -0700

    101-quotee.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main -Entry point 
+ * main - Entry point 
  *
  * Return: 1 (Success)
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, str1, 59);
 	return (1);
 }
