f1fef6a5f56f59c51a98385e0f3947a9a04be119
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-03 09:17:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Use putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdeghijklmnopqrstuvwxyz"
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\');
+	return (0);
+}
