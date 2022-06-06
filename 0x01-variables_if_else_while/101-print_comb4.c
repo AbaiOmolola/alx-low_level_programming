59421980d031bbea6f8ea197d03eafac5847cf84
AbaiOmolola <105018454+AbaiOmolola@users.noreply.github.com>
2022-06-06 23:14:29 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,20 +9,20 @@
 */
 int main(void)
 {
-        int i, e, g;
+int i, e, g;
 
-        i = 48;
-        e = 48;
-        g = 48;
+i = 48;
+e = 48;
+g = 48;
 
-        while (e < 58)
-        {
-                i = 48;
-                while (i < 58)
-                {
-                        g = 48;
-                        while (g < 58)
-                        {
+while (e < 58)
+{
+i = 48;
+while (i < 58)
+{
+g = 48;
+while (g < 58)
+{
                                 if (e != i && e != g && i != g && e < i && i < g)
                                 {
                                         putchar(e);
