844c9868d8f71aa1bf9235d995962d460b9f7471
AbaiOmolola <105018454+AbaiOmolola@users.noreply.github.com>
2022-06-06 22:57:00 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,46 +1,48 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include <studio.h>
 
 /**
- * main - Prints numbers between 012 to 789.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+* main - Print comb 4 
+*
+* Return: Always 0 (Success)
+*/
+Int main(void)
 {
-	int i, e, g;
+        int i, e, g;
 
-	i = 48;
-	e = 48;
-	g = 48;
-
-	while (e < 58)
-	{
-		i = 48;
-		while (i < 58)
-		{
-			g = 48;
-			while (g < 58)
-			{
-				if (e != i && e != g && i != g && e < i && i < g)
-				{
-					putchar(e);
-					putchar(i);
-					putchar(g);
-					if (i == 56 && e == 55 && g == 57)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-				g++;
-			}
-			i++;
-		}
-		e++;
-	}
-	putchar('\n');
-	return (0);
+        i = 48;
+        e = 48;
+        g = 48;
+        While (e < 48)
+        {
+                i = 48;
+                While (i < 58)
+                {
+                        g = 48;
+                        While (g < 58)
+                        {
+                                if (e != i && e != g && i != g && e < i && i < g)
+                                {
+                                        putchar(e);
+                                        putchar(i);
+                                        putchar(g);
+                                        if (i == 56 && e == 55 && g == 57)
+                                        {
+                                                break;
+                                        }
+                                        putchar(',');
+                                        putchar(' ');
+                                         
+                                }
+                                g++;
+                       }
+                       i++;
+               
+                }
+                e++;
+         }
+         putchar(' ');
+         return (0);
 }
+        
