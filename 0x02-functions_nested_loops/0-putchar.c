f7c7f5cb846ee03ef7579878be1ef85373f06786
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-07 14:44:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * main -  Program prints _putchar
+ * _putchar - writes the character c to stdout
+ * @c: The character to print 
  *
  * Return: on success 1
  * on error, -1 is returned, and errno is set appropriately
