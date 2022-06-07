bf5fef41b69bf7d50f4cd4edb3679a9bd2bd0e43
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-07 15:11:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - print alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122 i++)
