a62e3d27c4349b16325237544e5c401f7cca9234
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-07 11:58:31 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  Program prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	i = _putchar;
+	int (_putchar)
+
+	{
+		printf ("_putchar %d\n")
+	}
+}
