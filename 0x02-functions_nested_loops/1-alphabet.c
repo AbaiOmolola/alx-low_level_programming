10f776a604c2857b68a011cdaa587df8fafe5414
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-08 10:24:03 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet in lower case
+ * main - Print alphabet in lower case
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
 	int i;
 
