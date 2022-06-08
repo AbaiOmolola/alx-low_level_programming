aa36516930d89bb8fadcc692ed04a289e65d1ae9
AbaiOmolola <105018454+AbaiOmolola@users.noreply.github.com>
2022-06-08 23:35:49 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,7 +25,7 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d",n);
-			if (n !=n 98)
+			if (n != 98)
 			{
 				printf(",");
 			}
@@ -34,7 +34,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		print("98");
+		printf("98");
 	}
 	printf("\n");
 }
