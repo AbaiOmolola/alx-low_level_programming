14b530ec653cd87c0bc471c5e5b979c0878b5b6c
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-08 11:15:29 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - print x10 alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'.. 'z'; ch <= 10; ch++)
+	{
+		_putchar(h);
+	}
+	_putchar('\n');
+}
