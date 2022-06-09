5b2aeb144601c42dfe7e4a793686fff5dbc11256
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-09 15:36:31 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: input of time to print
+ * Return: a stright line
+ */
+void print_line(int n)
+{
+	int z;
+
+	if(n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (z = 1; z <= n; z++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
