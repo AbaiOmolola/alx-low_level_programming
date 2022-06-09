2b6961d9020563b2e7c303096f1014602febb418
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-09 06:30:32 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - test fnction that prints integer
+ * main - test function that prints integer
  *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 98;
 	positive_or_negative(i);
+
 	return (0);
 }
