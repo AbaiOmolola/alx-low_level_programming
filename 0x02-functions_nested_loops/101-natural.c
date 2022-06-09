1724db4d544d0338f87eaa6080b9fdd82965af92
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-08 17:12:25 -0700

    101-matural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - list all 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 | (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
