dd388752befb81d256fe196541f11f40a3ac41d2
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-09 04:41:57 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,12 @@
 /**
  * _isupper - checks for lower characters
  * @c: character to be checked
+ *
  * Return: Always 0 (Success)
  */
 int _isupper(int c)
 {
-	if (c = 'A'; c <= 'Z'; c++)
+	if (c = 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
