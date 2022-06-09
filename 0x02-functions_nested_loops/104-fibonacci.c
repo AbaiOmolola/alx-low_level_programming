3c231ad33218130b9a05eef3c82c56c63fa3d944
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-08 18:01:38 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print first 98
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 2;
+
+	long double fib1 = 1, fib2 = 2, sum;
+
+	printf("1, 2, ");
+
+	while (count < 98)
+	{
+		sum = fib1 = fib2;
+		printf("%.0Lf", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count  == 97)
+			printf("\n");
+		else
+			printf(", ");
+		count++;
+	}
+
+	return (0);
+}
