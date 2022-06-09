4b2b76ae94a411e411f7cd18fd0b97b0e89bc398
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-09 06:01:36 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 /**
- * m - check for upper case 
- *
+ * m - check for upper case
+ * @c: parameter of function
  * Return: Always 0 (Success)
  */
 int _isupper(int c)
 {
-	if (c >= '65' && c <= '90')
+	if(c >= 65 && c >= 90)
 	{
-		return(1);
+	return (1);
 	}
-
 	else
-		return(0);
+	return (0);
 }
