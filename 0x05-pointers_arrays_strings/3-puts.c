2faa869bc0f068d6fae7717a2d85c48c367a6fb0
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-13 15:44:32 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print string followed by a new line
+ * @str: parameter
+ *
+ * Return: Always 0
+ */
+void _puts(char *s)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
