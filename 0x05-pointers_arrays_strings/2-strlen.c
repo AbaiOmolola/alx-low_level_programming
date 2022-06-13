38aafb712c1717b182cfae41f89694a07b0979a9
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-13 14:34:42 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - function that returnslength of string 
+ * @s: length of string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return(c);
+}
