d7e28557b0e8be5d85ae9abf90be1ddfb5612e83
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-15 04:40:08 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: copy to
+ * @str: copy from
+ * Return: two concat strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest [i + n] = src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
