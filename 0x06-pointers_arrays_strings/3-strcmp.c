bda9c61c098622ab8168909bf7dbf5f12b57e9c8
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-16 05:58:53 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - compares two strings
  * @s1 - first string
  * @s2: second string
- *
  * Return: '-' diff, if s1 < s2
  * '0' if s1 == s2
  * '+' diff, if s1 > s2
@@ -16,6 +15,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-
 	return (*s1 - *s2);
 }
