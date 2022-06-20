0c506fa5b64fbd7e964f9da414020f08480bb86e
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-20 08:14:54 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,14 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int len = 0;
-
+	
 	if ((s == NULL) || (accept == NULL))
 		return (len);
-
+	
 	while (*s && strchr(accept,*s++))
 	{
 		len++;
 	}
-
+	
 	return (len);
 }
