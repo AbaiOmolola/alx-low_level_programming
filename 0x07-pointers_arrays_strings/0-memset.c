c49630c48a942a01d347eaa75cf86e88b2c7a9dd
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-20 07:29:34 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <string.h.
+#include <string.h>
 
 /**
  * _memset - function that fills memory with a constant byte
- * pointed to by s with the constant byte b
+ * pointed to by s with the constant byte
  * @s: pointer to the block of memeory to fill
  * @b: this is the value to be set
  * @n: this is the numbe rof byte to set to the value
