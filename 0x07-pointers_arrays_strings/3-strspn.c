5d5c6f17101f800143614af7d2afc704cf4eca41
AbaiOmolola <105018454+AbaiOmolola@users.noreply.github.com>
2022-06-20 14:47:22 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	while (s[i] !='\0')
 	{
 		t = 0;
-		for (j = 0; accept[j] !='\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 				t = 1;
