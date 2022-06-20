55b3345210c0a92bdae9ff977f0d6fd18041d9f5
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-20 05:12:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a = 0;
 
-	for (i = 0; i < n; i++)
-		*(s + i) = b;
-
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
 
 	return (s);
 }
