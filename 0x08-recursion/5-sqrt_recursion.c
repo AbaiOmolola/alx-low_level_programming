8188fe7d6201465587cadaa065fefb57ebca0471
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-23 13:56:09 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * power_operation - return the natural square root of a number 
+ * power_operation - return the natural square root of a number
  * @n: input number
  * @c: iterator
  * Return: square root or -1
@@ -18,7 +18,7 @@ int power_operation(int n, int c)
 	return (0 + power_operation(n, c + 1));
 }
 /**
- * _sqrt_recursion - return the natural square root of a number 
+ * _sqrt_recursion - return the natural square root of a number
  * @n: input number
  * Return: natural square root
  */
