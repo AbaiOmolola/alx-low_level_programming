6beda8ed1ef45fa435bc4ed77acc92ac6eb3388a
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-23 13:12:54 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * _strlen_recursion - returns the length of a string 
+ * _strlen_recursion - returns the length of a string
  * @s: string
  * Return: length of string
  */
