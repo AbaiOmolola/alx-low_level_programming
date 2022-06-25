c92de5131c8649b37bd56e5696c5c4b18be28c4a
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-25 13:59:02 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,7 +4,7 @@
  * main - prints the minimum number of coins
  * of money
  * @argc: argument count
- * @argv:argument vector
+ * @argv: argument vector
  * Return: Always 0
  */
 int main(int argc, char *argv[])
