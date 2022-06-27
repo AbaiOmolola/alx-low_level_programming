540168f6ea8237d0eac440ce374812472f1de808
AbaiOmolola <abaimaryomolola@gmail.com>
2022-06-27 09:29:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	if (!s2)
 		s2 = "";
 
-	for (size = 0; s1[size1] != '\0'; size1++)
+	for (size1 = 0; s1[size1] != '\0'; size1++)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 		;
