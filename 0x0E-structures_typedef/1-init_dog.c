cc28bbd097dec2d42267c7a218c5eaa60336461a
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-04 12:46:29 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#define NULL (void *)0
+/**
+ * init_dog - initializes dog structure 
+ *
+ * @d: struct to intialize
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: always void 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
