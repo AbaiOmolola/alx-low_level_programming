7cfa8b358ae90a5332624d4d513b8c0a535e3aaa
AbaiOmolola <105018454+AbaiOmolola@users.noreply.github.com>
2022-07-04 20:59:04 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d=> == NILL;
+		d=>name == NILL;
 	if (d->owner == NULL)
 		d->owner == NILL;
 	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
