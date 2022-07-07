0ffcb978ec333c293574c1807f5a21b26f8f7553
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-07 11:29:00 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - retuns the sum of parameters
+ * @n: number arguments
+ * Return: sum
+ * 0, if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list omolola;
+	int add = 0;
+	unsigned int x;
+
+	if (n == 0)
+		return (0);
+
+	va_start(omolola, n);
+
+	for (x = 0; x < n; x++)
+		add += va_arg(omolola, int);
+	va_end(kampala);
+	return (add);
+}
