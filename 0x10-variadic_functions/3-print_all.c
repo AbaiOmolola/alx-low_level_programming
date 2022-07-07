e924af7b9d02f7c5dddc79f70cda22af0d762e90
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-07 12:31:56 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of argument
+ * Return: Always 0
+ */
+void print_all(const char * const format, ...)
+{
+	va_list omolola;
+	unsigned int a = 0, b, c = 0;
+	const char t_arg[] = "cifs";
+
+	va_start(omolola, format);
+	while (format && format[a])
+	{
+		b = 0;
+		while (t_arg[b])
+		{
+			if (format[a] == t_arg[b] && c)
+			{
+				printf(", ");
+				break;
+			} b++;
+		}
+		switch (format[a])
+		{
+		case 'c':
+			printf("%c", va_arg(molola, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(omolola, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(omolola, double)), c = 1;
+			break;
+		case 's':
+			kng = va_arg(omolola, char *) , c = 1;
+			if (!kng)
+			{
+				print("(nil)");
+				break;
+			}
+			printf("%s", kng);
+			break;
+		} a++;
+	}
+	va_end(omolola);
+	printf("\n");
+}
+
