4ddc344b7794210a9971e55c3d65e08904ca82e7
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-07 11:45:38 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,9 +2,9 @@
 
 /**
  * print_numbers - print numbers
- * followed by a new line 
- * @separator: string to be printed 
- * @n: number of integers passed 
+ * followed by a new line
+ * @separator: string to be printed
+ * @n: number of integers passed
  * Return: Always 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
