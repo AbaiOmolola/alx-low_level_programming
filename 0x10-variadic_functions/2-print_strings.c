e708cf182ac5ea6f4334b8db79db05a7196b982a
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-07 12:00:50 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - print strings
+ * print_strings - print string
  * followed by a new line
  * @separator: string to be printed
  * @n: number of strings passed
@@ -12,14 +12,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list omolola;
 	unsigned int x;
 	char *kng;
-
+	
 	va_start(omolola, n);
 	for (x = 0; x < n; x++)
 	{
 		kng = va_arg(omolola, char *);
 		if (!kng)
 			printf("(nil)");
-		else 
+		else
 		{
 			printf("%s", kng);
 			if (x < n - 1 && separator)
