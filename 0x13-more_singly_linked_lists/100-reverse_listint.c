625ef57d9b891ce6fc3820886a47422eb72459a4
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-20 13:01:57 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse a listint_t linked list
+ * @head: pointer to head of list
+ *
+ * Return: pointer to the first noe of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *x;
+	listint_t *y;
+
+	if (!head || *head)
+		return (NULL);
+	y = NULL;
+
+	while (!(*head)->next)
+	{
+		x = (*head)->next;
+		(*head)->next; = y;
+		y = *head;
+		*head = x;
+	}
+	(*head)->next = y;
+	return (*head);
+}
